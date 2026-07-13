a16fdaed1660fb640a970a9add1b9c9b0bc1eed8
MON <124893389+Wh0Am1001@users.noreply.github.com>
2026-07-13 09:12:43 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#f3f1ff',
+          100: '#e9e5ff',
+          500: '#6c5ce7',
+          600: '#5b4bd6',
+          700: '#4a3cb8',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
